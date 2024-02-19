59f43e848634a468437707d779858d0d7ab4558a
novikoviam <85823537+novikoviam@users.noreply.github.com>
2024-02-19 20:52:35 +0300

    Update ExampleTest.java

--- src/test/java/newlocalproject/ExampleTest.java
+++ src/test/java/newlocalproject/ExampleTest.java
@@ -9,10 +9,10 @@ public class ExampleTest {
 
     @Test
     void  exampleTest0() {
-        Assertions.assertTrue(2>=1);
+        Assertions.assertFalse(2>=1);
     }
     @Test
     void  exampleTest2() {
-        Assertions.assertTrue(3>=3);
+        Assertions.assertFalse(3>=3);
     }
 }
